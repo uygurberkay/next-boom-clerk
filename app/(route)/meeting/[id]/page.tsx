a3690237b09ed3a6e579c3944ab374038dea62c7
Berkay Uygur <50241715+uygurberkay@users.noreply.github.com>
2024-04-19 16:44:33 +0300

    first-commitment

---
+++ app/(route)/meeting/[id]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface MeetingProps  {
+    id: string;
+}
+
+const Meeting = ({ params }: { params: { id: string } }) => {
+    return (
+        <div>
+            Meeting Room : #{params.id}
+        </div>
+    )
+}
+
+export default Meeting

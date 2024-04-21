585618139505c5f72c5e5c3fea6af188f6928cac
Berkay Uygur <50241715+uygurberkay@users.noreply.github.com>
2024-04-21 13:59:22 +0300

    personal-room-page-done

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -80,7 +80,7 @@ const CallList = ({ type }: { type:  CallListProps}) => {
                     title={
                         (meeting as Call).state?.custom?.description ||
                         (meeting as CallRecording).filename?.substring(0, 20) ||
-                        'No Description'
+                        'Personal Meeting'
                     }
                     date={
                         (meeting as Call).state?.startsAt?.toLocaleString() ||
